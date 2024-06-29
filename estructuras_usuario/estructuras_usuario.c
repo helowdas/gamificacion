c79b9e8e81e394dd53fc54bcfa72ddbf0e4e8776
Jean Paul <jeanpaulcora@gmail.com>
2024-06-29 19:08:47 -0400

    añadido struct user log, optimizacion structs actividades

--- estructuras_usuario/estructuras_usuario.c
+++ estructuras_usuario/estructuras_usuario.c
@@ -1,7 +1,12 @@
 # include <stdio.h>
 
 // ESTRUCTURAS DE USUARIO
+typedef struct
+{
+    char User_name[50];
+    int password;
 
+}Userlog;
 
 typedef struct
 {
@@ -42,115 +47,15 @@ typedef struct activity1
     struct activity1* left;
     struct activity1* right;
 
-}actividad_correr;
+}actividad_type1;
 
-typedef struct activity2 
-{
-    estructura_tiempo tiempo;
-    int distancia;
-    struct activity2* left;
-    struct activity2* right;
-
-
-}actividad_caminar;
-
-typedef struct activity3
-{
-    estructura_tiempo tiempo;
-    int distancia;
-    struct activity3* left;
-    struct activity3* right;
-
-}actividad_nadar;
-
-typedef struct activity4
-{
-    estructura_tiempo tiempo;
-    int distancia;
-    struct activity4* left;
-    struct activity4* right;
-
-}actividad_ciclismo;
-
-typedef struct activity5
-{
-    estructura_tiempo tiempo;
-    struct activity5* left;
-    struct activity5* right;
-
-}actividad_yoga;
-
-typedef struct activity6
-{
-    estructura_tiempo tiempo;
-    struct activity6* left;
-    struct activity6* right;
-
-}actividad_crossfit;
-
-typedef struct activity7
-{
-    estructura_tiempo tiempo;
-    struct activity7* left;
-    struct activity7* right;
-
-}actividad_pilates;
-
-typedef struct activity8
-{
-    estructura_tiempo tiempo;
-    struct activity8* left;
-    struct activity8* right;
-
-}actividad_tenis;
-
-typedef struct activity9
-{
-    estructura_tiempo tiempo;
-    struct activity9* left;
-    struct activity9* right;
-
-}actividad_futbol;
-
-typedef struct activity10
-{
-    estructura_tiempo tiempo;
-    struct activity10* left;
-    struct activity10* right;
-
-}actividad_artes_marciales;
-
-typedef struct activity11
-{
-    estructura_tiempo tiempo;
-    struct activity11* left;
-    struct activity11* right;
-
-}actividad_levantamiento_pesas;
-
-typedef struct activity12
-{
-    estructura_tiempo tiempo;
-    struct activity12* left;
-    struct activity12* right;
-
-}actividad_voleibol;
-
-typedef struct activity13
-{
-    estructura_tiempo tiempo;
-    struct activity13* left;
-    struct activity13* right;
-
-}actividad_padel;
-
-typedef struct activity14
+typedef struct activity2
 {
     estructura_tiempo tiempo;
     struct activity14* left;
     struct activity14* right;
 
-}actividad_calistenia;
+}actividad_type2;
 
 // ESTRUCTURAS LOGROS
 
@@ -167,20 +72,20 @@ typedef struct estructura_logros_usuario
 
 typedef struct estructura_activity_tree
 {
-    actividad_caminar* caminar;
-    actividad_correr* correr;
-    actividad_crossfit* crossfit;
-    actividad_artes_marciales* artes_marciales;
-    actividad_ciclismo* ciclismo;
-    actividad_futbol* futbol;
-    actividad_levantamiento_pesas* levantamiento_pesas;
-    actividad_nadar* nadar;
-    actividad_padel* padel;
-    actividad_pilates* pilates;
-    actividad_tenis* tenis;
-    actividad_voleibol* voleibol;
-    actividad_yoga* yoga;
-    actividad_calistenia* calistenia;
+    actividad_type1* caminar;
+    actividad_type1* correr;
+    actividad_type2* crossfit;
+    actividad_type2* artes_marciales;
+    actividad_type1* ciclismo;
+    actividad_type2* futbol;
+    actividad_type2* levantamiento_pesas;
+    actividad_type1* nadar;
+    actividad_type2* padel;
+    actividad_type2* pilates;
+    actividad_type2* tenis;
+    actividad_type2* voleibol;
+    actividad_type2* yoga;
+    actividad_type2* calistenia;
     
 }activity_tree;
 
