81f6603b6f50cda7317f8f4f8da815b562a426ba
Jean Paul <jeanpaulcora@gmail.com>
2024-06-29 19:28:05 -0400

    cambios

--- funciones_informe/func_informes.c
+++ funciones_informe/func_informes.c
@@ -53,7 +53,7 @@ char mes_name(int mes)
 
 // FUNCIONES DE RECORRIDO DE ARBOLES
 
-void preOrden_marcial(actividad_artes_marciales* nodo, int* hour_monthly, int mes, int año)
+void preOrden_activity_type2(actividad_type2* nodo, int* hour_monthly, int mes, int year)
 {
     if (nodo == NULL)
         return -1;
@@ -62,16 +62,16 @@ void preOrden_marcial(actividad_artes_marciales* nodo, int* hour_monthly, int me
     {
         mes = 13;
     }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
+    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == year)
     {
         *hour_monthly += nodo->tiempo.duracion_hour;
     }
 
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
+    preOrden_activity_type2(nodo->left, hour_monthly, mes, year);
+    preOrden_activity_type2(nodo->right, hour_monthly, mes, year);
 }
 
-void preOrden_calistenia(actividad_calistenia* nodo, int* hour_monthly, int mes, int año)
+void preOrden_activity_type_1(actividad_type1* nodo, int* hour_monthly, int mes, int year)
 {
     if (nodo == NULL)
         return -1;
@@ -80,267 +80,52 @@ void preOrden_calistenia(actividad_calistenia* nodo, int* hour_monthly, int mes,
     {
         mes = 13;
     }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
+    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == year)
     {
         *hour_monthly += nodo->tiempo.duracion_hour;
     }
 
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
+    preOrden_activity_type_1(nodo->left, hour_monthly, mes, year);
+    preOrden_activity_type_1(nodo->right, hour_monthly, mes, year);
 }
 
-void preOrden_caminar(actividad_caminar* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_ciclismo(actividad_ciclismo* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_correr(actividad_correr* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_crossfit(actividad_crossfit* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_futbol(actividad_futbol* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_pesas(actividad_levantamiento_pesas* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_nadar(actividad_nadar* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_padel(actividad_padel* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_pilates(actividad_pilates* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_tenis(actividad_tenis* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_voleibol(actividad_voleibol* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
-
-void preOrden_yoga(actividad_yoga* nodo, int* hour_monthly, int mes, int año)
-{
-    if (nodo == NULL)
-        return -1;
-
-    if(mes == 1)
-    {
-        mes = 13;
-    }
-    if(nodo->tiempo.month = mes-1 && nodo->tiempo.year == año)
-    {
-        *hour_monthly += nodo->tiempo.duracion_hour;
-    }
-
-    preOrden(nodo->left, hour_monthly, mes);
-    preOrden(nodo->right, hour_monthly, mes);
-}
 
 // FUNCIONES INFORMEN HORAS ACTIVIDAD FISICA MENSUAL
 
-int informe_horas_mensuales(main_tree* arbol, int mes, int año)
+int informe_horas_mensuales(main_tree* arbol, int mes, int year)
 {
     int hour_monthly = 0;
     
-    preOrden_calistenia(arbol->actividades->calistenia, &hour_monthly, mes, año);
-    preOrden_marcial(arbol->actividades->artes_marciales, &hour_monthly, mes, año);
-    preOrden_caminar(arbol->actividades->caminar, &hour_monthly, mes, año);
-    preOrden_ciclismo(arbol->actividades->ciclismo, &hour_monthly, mes, año);
-    preOrden_correr(arbol->actividades->correr, &hour_monthly, mes, año);
-    preOrden_crossfit(arbol->actividades->crossfit, &hour_monthly, mes, año);
-    preOrden_futbol(arbol->actividades->futbol, &hour_monthly, mes, año);
-    preOrden_pesas(arbol->actividades->levantamiento_pesas, &hour_monthly, mes, año);
-    preOrden_nadar(arbol->actividades->nadar, &hour_monthly, mes, año);
-    preOrden_padel(arbol->actividades->padel, &hour_monthly, mes, año);
-    preOrden_pilates(arbol->actividades->pilates, &hour_monthly, mes, año);
-    preOrden_tenis(arbol->actividades->tenis, &hour_monthly, mes, año);
-    preOrden_voleibol(arbol->actividades->voleibol, &hour_monthly, mes, año);
-    preOrden_yoga(arbol->actividades->yoga, &hour_monthly, mes, año);
+    preOrden_activity_type2(arbol->actividades->calistenia, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->artes_marciales, &hour_monthly, mes, year);
+    preOrden_activity_type_1(arbol->actividades->caminar, &hour_monthly, mes, year);
+    preOrden_activity_type_1(arbol->actividades->ciclismo, &hour_monthly, mes, year);
+    preOrden_activity_type_1(arbol->actividades->correr, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->crossfit, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->futbol, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->levantamiento_pesas, &hour_monthly, mes, year);
+    preOrden_activity_type_1(arbol->actividades->nadar, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->padel, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->pilates, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->tenis, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->voleibol, &hour_monthly, mes, year);
+    preOrden_activity_type2(arbol->actividades->yoga, &hour_monthly, mes, year);
 
     int hour_monthly2 = 0;
-    preOrden_calistenia(arbol->actividades->calistenia, &hour_monthly2, mes-1, año);
-    preOrden_marcial(arbol->actividades->artes_marciales, &hour_monthly2, mes-1, año);
-    preOrden_caminar(arbol->actividades->caminar, &hour_monthly2, mes-1, año);
-    preOrden_ciclismo(arbol->actividades->ciclismo, &hour_monthly2, mes-1, año);
-    preOrden_correr(arbol->actividades->correr, &hour_monthly2, mes-1, año);
-    preOrden_crossfit(arbol->actividades->crossfit, &hour_monthly2, mes-1, año);
-    preOrden_futbol(arbol->actividades->futbol, &hour_monthly2, mes-1, año);
-    preOrden_pesas(arbol->actividades->levantamiento_pesas, &hour_monthly2, mes-1, año);
-    preOrden_nadar(arbol->actividades->nadar, &hour_monthly2, mes-1, año);
-    preOrden_padel(arbol->actividades->padel, &hour_monthly2, mes-1, año);
-    preOrden_pilates(arbol->actividades->pilates, &hour_monthly2, mes-1, año);
-    preOrden_tenis(arbol->actividades->tenis, &hour_monthly2, mes-1, año);
-    preOrden_voleibol(arbol->actividades->voleibol, &hour_monthly2, mes-1, año);
-    preOrden_yoga(arbol->actividades->yoga, &hour_monthly2, mes-1, año);
+    preOrden_activity_type2(arbol->actividades->calistenia, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->artes_marciales, &hour_monthly2, mes, year);
+    preOrden_activity_type_1(arbol->actividades->caminar, &hour_monthly2, mes, year);
+    preOrden_activity_type_1(arbol->actividades->ciclismo, &hour_monthly2, mes, year);
+    preOrden_activity_type_1(arbol->actividades->correr, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->crossfit, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->futbol, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->levantamiento_pesas, &hour_monthly2, mes, year);
+    preOrden_activity_type_1(arbol->actividades->nadar, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->padel, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->pilates, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->tenis, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->voleibol, &hour_monthly2, mes, year);
+    preOrden_activity_type2(arbol->actividades->yoga, &hour_monthly2, mes, year);
 
     printf("\n-----------------------------------------------------------\n");
     printf("   Tus Horas de actividad fisica del mes %s fueron: %d\n", mes_name((mes==1)?12:mes-1), hour_monthly);
--- funciones_informe/func_informes.h
+++ funciones_informe/func_informes.h
@@ -6,35 +6,10 @@ char mes_name(int mes);
 
 // FUNCIONES DE RECORRIDO DE ARBOLES
 
-void preOrden_marcial(actividad_artes_marciales* nodo, int* hour_monthly, int mes, int año);
+void preOrden_activity_type2(actividad_type2* nodo, int* hour_monthly, int mes, int year);
 
-void preOrden_calistenia(actividad_calistenia* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_caminar(actividad_caminar* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_ciclismo(actividad_ciclismo* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_correr(actividad_correr* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_crossfit(actividad_crossfit* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_futbol(actividad_futbol* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_pesas(actividad_levantamiento_pesas* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_nadar(actividad_nadar* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_padel(actividad_padel* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_pilates(actividad_pilates* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_tenis(actividad_tenis* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_voleibol(actividad_voleibol* nodo, int* hour_monthly, int mes, int año);
-
-void preOrden_yoga(actividad_yoga* nodo, int* hour_monthly, int mes, int año);
-
+void preOrden_activity_type_1(actividad_type1* nodo, int* hour_monthly, int mes, int year);
 
 // FUNCIONES INFORMEN HORAS ACTIVIDAD FISICA MENSUAL
 
-int informe_horas_mensuales(main_tree* arbol, int mes, int año);
+int informe_horas_mensuales(main_tree* arbol, int mes, int year);
