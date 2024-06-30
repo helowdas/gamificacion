e9a3013c2245273b8241aa8ce10b41cfee047d76
Jean Paul <jeanpaulcora@gmail.com>
2024-06-29 22:13:58 -0400

    añadido funcion horas totales por actividad

--- funciones_informe/func_informes.c
+++ funciones_informe/func_informes.c
@@ -53,7 +53,7 @@ char mes_name(int mes)
 
 // FUNCIONES DE RECORRIDO DE ARBOLES
 
-void preOrden_activity_type2(actividad_type2* nodo, int* hour_monthly, int mes, int year)
+int preOrden_activity_type2(actividad_type2* nodo, int* hour_monthly, int mes, int year)
 {
     if (nodo == NULL)
         return -1;
@@ -71,7 +71,7 @@ void preOrden_activity_type2(actividad_type2* nodo, int* hour_monthly, int mes, 
     preOrden_activity_type2(nodo->right, hour_monthly, mes, year);
 }
 
-void preOrden_activity_type_1(actividad_type1* nodo, int* hour_monthly, int mes, int year)
+int preOrden_activity_type_1(actividad_type1* nodo, int* hour_monthly, int mes, int year)
 {
     if (nodo == NULL)
         return -1;
@@ -92,7 +92,7 @@ void preOrden_activity_type_1(actividad_type1* nodo, int* hour_monthly, int mes,
 
 // FUNCIONES INFORMEN HORAS ACTIVIDAD FISICA MENSUAL
 
-int informe_horas_mensuales(main_tree* arbol, int mes, int year)
+void informe_horas_mensuales(main_tree* arbol, int mes, int year)
 {
     int hour_monthly = 0;
     
@@ -112,6 +112,7 @@ int informe_horas_mensuales(main_tree* arbol, int mes, int year)
     preOrden_activity_type2(arbol->actividades->yoga, &hour_monthly, mes, year);
 
     int hour_monthly2 = 0;
+
     preOrden_activity_type2(arbol->actividades->calistenia, &hour_monthly2, mes, year);
     preOrden_activity_type2(arbol->actividades->artes_marciales, &hour_monthly2, mes, year);
     preOrden_activity_type_1(arbol->actividades->caminar, &hour_monthly2, mes, year);
@@ -129,7 +130,7 @@ int informe_horas_mensuales(main_tree* arbol, int mes, int year)
 
     printf("\n-----------------------------------------------------------\n");
     printf("   Tus Horas de actividad fisica del mes %s fueron: %d\n", mes_name((mes==1)?12:mes-1), hour_monthly);
-    printf("\n-----------------------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
 
     if(hour_monthly > hour_monthly2)
     {
@@ -138,5 +139,260 @@ int informe_horas_mensuales(main_tree* arbol, int mes, int year)
         printf("\033[33m\n********************\n\033[0m\n");
     
     }
+
+    
 }
+
+// Funcion horas de cada actividad de usuario
+
+int horas_actividad_type2(actividad_type2* nodo, int* horas)
+{
+    if (nodo == NULL)
+        return -1;
+
+    *horas += nodo->tiempo.duracion_hour; 
+
+    horas_actividad_type2(nodo->left, horas);
+    horas_actividad_type2(nodo->right, horas);
+}
+
+int Horas_actividad_type1(actividad_type1* nodo, int* horas)
+{
+    if (nodo == NULL)
+        return -1;
+
+    *horas += nodo->tiempo.duracion_hour;
+
+    horas_actividad_type2(nodo->left, horas);
+    horas_actividad_type2(nodo->right, horas);
+}
+
+void actividades_usuario_informe (main_tree* arbol)
+{
+    int contador = 0;
+    int actividad[14] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    printf("\n---------- ACTIVIDADES FISICAS QUE HAS REALIZADO ----------\n");
+
+    if(arbol->actividades->artes_marciales != NULL)
+    {
+        contador += 1;
+        actividad[0] = contador;
+        printf("_%d. Artes Marciales\n");
+    }
+    if(arbol->actividades->calistenia != NULL)
+    {
+        contador += 1;
+        actividad[1] = contador;
+        printf("_%d. Calistenia\n", contador);
+    }
+    if(arbol->actividades->caminar != NULL)
+    {
+        contador += 1;
+        actividad[2] = contador;
+        printf("_%d. Caminar\n", contador);
+    }
+    if(arbol->actividades->ciclismo != NULL)
+    {
+        contador += 1;
+        actividad[3] = contador;
+        printf("_%d. Ciclismo\n", contador);
+    }
+    if(arbol->actividades->correr != NULL)
+    {
+        contador += 1;
+        actividad[4] = contador;
+        printf("_%d. Correr\n", contador);
+    }
+    if(arbol->actividades->crossfit != NULL)
+    {
+        contador += 1;
+        actividad[5] = contador;
+        printf("_%d. Crossfit\n", contador);
+    }
+    if(arbol->actividades->futbol != NULL)
+    {
+        contador += 1;
+        actividad[6] = contador;
+        printf("_%d. Futbol\n", contador);
+    }
+    if(arbol->actividades->levantamiento_pesas != NULL)
+    {
+        contador += 1;
+        actividad[7] = contador;
+        printf("_%d. Levantamiento de Pesas\n", contador);
+    }
+    if(arbol->actividades->nadar != NULL)
+    {
+        contador += 1;
+        actividad[8] = contador;
+        printf("_%d. Nadar\n", contador);
+    }
+    if(arbol->actividades->padel != NULL)
+    {
+        contador += 1;
+        actividad[9] = contador;
+        printf("_%d. Padel\n", contador);
+    }
+    if(arbol->actividades->pilates != NULL)
+    {
+        contador += 1;
+        actividad[10] = contador;
+        printf("_%d. Pilates\n", contador);
+    }
+    if(arbol->actividades->tenis != NULL)
+    {
+        contador += 1;
+        actividad[11] = contador;
+        printf("_%d. Tenis\n", contador);
+    }
+    if(arbol->actividades->voleibol != NULL)
+    {
+        contador += 1;
+        actividad[12] = contador;
+        printf("_%d. Voleibol\n", contador);
+    }
+    if(arbol->actividades->yoga != NULL)
+    {
+        contador += 1;
+        actividad[13] = contador;
+        printf("_%d. Yoga\n", contador);
+    }
+
+    contador += 1;
+    printf("_%d. Salir\n", contador);
+
+    int horas_actividad = 0;
+    int opcion;
+    printf("----- INSERTE EL NUMERO DE LA ACTIVIDAD QUE QUIERA\n");
+    printf("----- SABER LAS HORAS REALIZADAS\n");
+    printf("-----:  ");
+    printf("\n");
+    scanf("%d", &opcion);
+
+    if(opcion == actividad[0])
+    {
+        Horas_actividad_type2(arbol->actividades->artes_marciales, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Artes Marciales son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[1])
+    {
+        Horas_actividad_type2(arbol->actividades->calistenia, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Calistenia son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[2])
+    {
+        Horas_actividad_type1(arbol->actividades->caminar, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Caminar son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[3])
+    {
+        Horas_actividad_type1(arbol->actividades->ciclismo, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Ciclismo son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[4])
+    {
+        Horas_actividad_type1(arbol->actividades->correr, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Correr son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[5])
+    {
+        Horas_actividad_type2(arbol->actividades->crossfit, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Crossfit son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[6])
+    {
+        Horas_actividad_type2(arbol->actividades->futbol, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Futbol son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[7])
+    {
+        Horas_actividad_type2(arbol->actividades->levantamiento_pesas, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Levantamiento de Pesas son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[8])
+    {
+        Horas_actividad_type1(arbol->actividades->nadar, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Nadar son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[9])
+    {
+        Horas_actividad_type2(arbol->actividades->padel, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Padel son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[10])
+    {
+        Horas_actividad_type2(arbol->actividades->pilates, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Pilates son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[11])
+    {
+        Horas_actividad_type2(arbol->actividades->tenis, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Tenis son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[12])
+    {
+        Horas_actividad_type2(arbol->actividades->voleibol, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Voleibol son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == actividad[13])
+    {
+        Horas_actividad_type2(arbol->actividades->yoga, &horas_actividad);
+        printf("\n-----------------------------------------------------------\n");
+        printf("   Tus Horas Totales de Yoga son: %d\n", horas_actividad);
+        printf("-----------------------------------------------------------\n");
+        actividades_usuario_informe (arbol);
+    }
+    else if(opcion == contador )
+    {
+        return;
+    }
+    else
+    {
+        printf("*** OPCION INVALIDA ***\n");
+        actividades_usuario_informe (arbol);
+
+    }
+
+}
+
+
 
